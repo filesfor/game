51e0727ce24bbda3e396b3ec837206cbfebd8c74
filesfor <143867560+filesfor@users.noreply.github.com>
2024-08-28 12:21:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,31 @@
-// Initialize the server
 var app = require('express')();
 var express = require('express');
 var path = require('path');
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-// Serve the /client folder
 var htmlPath = path.join(__dirname, 'client');
 app.use(express.static(htmlPath));
 
-// Variable that stores the players
 const gameState = {
   players: {}
 };
 
-// Function that is called whenever someone joins
 io.on('connection', (socket) => {
   socket.on('newPlayer', (playerName) => {
-    // Someone joined!
+    console.log("Someone joined!")
     gameState.players[socket.id] = {
       x: 250,
       y: 250,
-      name: playerName || 'Anonymous' // Set player name or default to 'Anonymous'
+      name: playerName || 'Anonymous'
     };
   });
 
   socket.on('playerMovement', (playerMovement) => {
-    // Someone Moved!
     const player = gameState.players[socket.id];
-    // These are boundaries
     const canvasWidth = 1200;
     const canvasHeight = 700;
 
-    // Use the object to move the player's coordinates
     if (playerMovement.left && player.x > 0) {
       player.x -= 4;
     }
@@ -48,17 +41,14 @@ io.on('connection', (socket) => {
   });
 
   socket.on("disconnect", () => {
-    // When someone leaves, remove them from the game state
     delete gameState.players[socket.id];
   });
 });
 
-// Emit the game state to the clients 60 times / second
 setInterval(() => {
   io.sockets.emit('state', gameState);
 }, 1000 / 60);
 
-// Start the server on port 3000
 http.listen(3000, () => {
   console.log('listening on *:3000');
 });
