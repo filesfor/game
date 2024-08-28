aa933b598223161bf298ad2ce852d7181edf914c
filesfor <143867560+filesfor@users.noreply.github.com>
2024-08-28 14:50:03 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -6,24 +6,28 @@ const ctx = canvas.getContext("2d");
 document.getElementById("game").width = document.documentElement.clientWidth;
 document.getElementById("game").height = document.documentElement.clientHeight;
 
-let playerSprite = new Image();
-playerSprite.src = 'https://filesfor.github.io/bean.png'; // Default sprite URL
+let playerName;
+do {
+    playerName = prompt("Enter your name:");
+} while (!isValidName(playerName));
+
+socket.emit('newPlayer', playerName);
+
+const playerSprite = new Image();
+playerSprite.src = 'https://filesfor.github.io/sprite1.png';
 
 playerSprite.onload = function() {
     socket.on('state', (gameState) => {
-      ctx.clearRect(0, 0, document.documentElement.clientWidth, document.documentElement.clientHeight);
-      for (let id in gameState.players) {
-        drawPlayer(gameState.players[id]);
-      }
+        ctx.clearRect(0, 0, document.documentElement.clientWidth, document.documentElement.clientHeight);
+        for (let id in gameState.players) {
+            drawPlayer(gameState.players[id]);
+        }
     });
 };
 
 function drawPlayer(player) {
-    const playerImage = new Image();
-    playerImage.src = player.sprite;
-
-    playerImage.onload = () => {
-        ctx.drawImage(playerImage, player.x, player.y, 50, 50);
+    if (playerSprite.complete && playerSprite.naturalHeight !== 0) {
+        ctx.drawImage(playerSprite, player.x, player.y, 50, 50);
 
         ctx.font = '16px Arial';
         ctx.textAlign = 'center';
@@ -33,5 +37,21 @@ function drawPlayer(player) {
 
         ctx.strokeText(player.name, player.x + 25, player.y - 10);
         ctx.fillText(player.name, player.x + 25, player.y - 10);
+    } else {
+        console.error("Image not loaded or failed to load.");
     }
 }
+
+function isValidName(name) {
+    const namePattern = /^[a-zA-Z0-9!@\$%\*]{1,20}$/;
+    return namePattern.test(name);
+}
+
+socket.on('invalidName', (message) => {
+    alert(message);
+    let newName;
+    do {
+        newName = prompt("Enter a different name:");
+    } while (!isValidName(newName));
+    socket.emit('newPlayer', newName);
+});
